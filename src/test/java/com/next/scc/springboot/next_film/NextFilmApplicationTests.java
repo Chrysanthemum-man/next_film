7f4a1bfb9c2d86040e6b282eb8354e4300bbd908
scc <876121638@qq.com>
2020-03-05 16:26:59 +0800

    first commit

---
+++ src/test/java/com/next/scc/springboot/next_film/NextFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.scc.springboot.next_film;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextFilmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
